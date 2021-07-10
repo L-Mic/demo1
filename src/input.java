1e0871549b6f1194e88a65007b136977a685a7c7
L-Mic <1726557110@qq.com>
2021-07-10 15:53:08 +0800

    input_test

---
+++ src/input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String a = input.next();
+        System.out.println(a + 'a' + '/');
+        int b = input.nextInt();
+        System.out.println(b*4);
+    }
+}
