cfed66b8e8de5509072120638232a281c2e4d7ba
L-Mic <1726557110@qq.com>
2021-07-12 15:48:24 +0800

    7.12

--- src/string1.java
+++ src/string1.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class string1 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        boolean even = num%2 ==0;
+        System.out.println(even);
         String a = "djfka;fk";
         System.out.println(a);
+
     }
 }
