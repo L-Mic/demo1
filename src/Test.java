0eb361d6a0b4ccf696b6980e9dddf9576b7a3b11
L-Mic <1726557110@qq.com>
2021-07-12 10:35:30 +0800

    7.12

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,10 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        int x = 2;
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.println(++x);
+        System.out.println(Math.pow(x,3));
     }
 }
--- src/input.java
+++ src/input.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class input {
     public static void main(String[] args) {
+        final double PI = 3.14;
         Scanner input = new Scanner(System.in);
         String a = input.next();
         System.out.println(a + 'a' + '/');
         int b = input.nextInt();
-        System.out.println(b*4);
+        System.out.println(b*PI);
     }
 }
